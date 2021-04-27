2fd4f88b7750e1564ee072e99c620f9660c4ce38
Alexander <alexquirozaldave@gmail.com>
2021-04-27 10:53:59 -0500

    Making the components more responsive

--- src/pages/Reports.js
+++ src/pages/Reports.js
@@ -32,18 +32,20 @@ function Reports() {
 
         </div>
 
-        <div className="chart-card">
-            <div className="chart-card-title">
-              <h3>Test Chart 3</h3>
-            </div>
-            <div className="chart-card-body">
-              <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Ad, molestias, officia eligendi delectus saepe temporibus nihil at distinctio vero a pariatur quia, 
-                unde eveniet aspernatur expedita consequuntur voluptatum reiciendis ex?</h2>
-              <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                Ad, molestias, officia eligendi delectus saepe temporibus nihil at distinctio vero a pariatur quia, 
-                unde eveniet aspernatur expedita consequuntur voluptatum reiciendis ex?</h2>
-            </div>
+        <div className="second-chart-card">
+          <div className="chart-card">
+              <div className="chart-card-title">
+                <h3>Test Chart 3</h3>
+              </div>
+              <div className="chart-card-body">
+                <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                  Ad, molestias, officia eligendi delectus saepe temporibus nihil at distinctio vero a pariatur quia, 
+                  unde eveniet aspernatur expedita consequuntur voluptatum reiciendis ex?</h2>
+                <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                  Ad, molestias, officia eligendi delectus saepe temporibus nihil at distinctio vero a pariatur quia, 
+                  unde eveniet aspernatur expedita consequuntur voluptatum reiciendis ex?</h2>
+              </div>
+          </div>
         </div>
 
       </div>
